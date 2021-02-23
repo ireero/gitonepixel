fd204907b85057e6c4a8662c64ce50e7fe3b0697
ireero <78822225+ireero@users.noreply.github.com>
2021-02-22 23:55:20 -0300

    At

---
+++ One Pixel/Assets/scripts/SteamManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamManager : MonoBehaviour
+{
+    private void Awake() {
+
+        try {
+            Steamworks.SteamClient.Init(1541410);
+        }
+
+        catch (System.Exception e){
+            Debug.Log("Não deu pra iniciar o steam client");
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnDisable() {
+        Steamworks.SteamClient.Shutdown();
+    }
+
+    void Update() {
+        Steamworks.SteamClient.RunCallbacks();    
+    }
+}
--- One Pixel/Assets/scripts/UI/MenuInicial.cs
+++ One Pixel/Assets/scripts/UI/MenuInicial.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuInicial : MonoBehaviour
 {
+
+    public Button txt_play;
+    public Button txt_tutorial;
+    public Button txt_sobreviver;
+    public Text txt_recorde_sobreviver;
+    public Text txt_recorde;
+
+    // Potugues
+    private string txt_play_br = "Jogar";
+    private string txt_tutorial_br = "Tutorial";
+    private string txt_sobreviver_br = "Sobreviver";
+    private string txt_recorde_br = "Recorde: ";
+    private string txt_recorde_sobreviver_br = "Recorde Sobreviver: ";
+    
     void Start()
     {
         Time.timeScale = 1;
@@ -13,7 +28,17 @@ public class MenuInicial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Application.systemLanguage == SystemLanguage.Portuguese) {
+
+            txt_play.GetComponentInChildren<Text>().text = txt_play_br;
+            txt_tutorial.GetComponentInChildren<Text>().text = txt_tutorial_br;
+            txt_sobreviver.GetComponentInChildren<Text>().text = txt_sobreviver_br;
+
+            txt_recorde_sobreviver.text = txt_recorde_sobreviver_br;
+            txt_recorde.text = txt_recorde_br;
+        }
+
     }
 
     public void Jogar() {
